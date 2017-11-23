986788e240a8eccbafdedf12d294f50870751c50
Vincent Palatin <vpalatin@chromium.org>
2017-11-23 15:45:07 +0100

    Add Docker scripts to build the Twinkie version
    Quick and dirty docker scripts to build the Sigrok/Pulseview
    binaries/packages with the Twinkie patches.

---
+++ cross-compile/sigrok-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EXE="sigrok-cli"
+BINDIR="$(dirname "$(readlink -f "$0")")"
+LIBDIR="$(readlink -f "${BINDIR}/../lib")"
+DECDIR="$(readlink -f "${BINDIR}/../share/libsigrokdecode/decoders")"
+REALBIN="${BINDIR}/${EXE}.real"
+
+LD_LIBRARY_PATH="${LIBDIR}" SIGROKDECODE_DIR="${DECDIR}" "${REALBIN}" "$@"
